684a92454e93c234d9a29d95fcda3f093ca2f796
Nihal <nihalybaig@ngenux.com>
2023-03-19 22:09:01 +0530

    mobile styling

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -46,29 +46,29 @@ const HOT_CARD_DETAILS = [
 const CATEGORIES = [
   {
     name: "Music",
-    imgUrl: `concerts`
+    imgUrl: `concerts`,
   },
   {
     name: "Sports",
-    imgUrl: `sport`
+    imgUrl: `sport`,
   },
   {
     name: "Theatre",
-    imgUrl: `theatre`
+    imgUrl: `theatre`,
   },
   {
     name: "Comedy",
-    imgUrl: `comedy`
+    imgUrl: `comedy`,
   },
   {
     name: "Food & drink",
-    imgUrl: `food-and-drink`
+    imgUrl: `food-and-drink`,
   },
   {
     name: "Exhibition",
-    imgUrl: `exhibition`
-  }
-]
+    imgUrl: `exhibition`,
+  },
+];
 export default function HomePage() {
   const [navbar, setNavbar] = useState(false);
 
@@ -248,143 +248,148 @@ export default function HomePage() {
         </div>
       </header>
 
-      <section
-        id="carousel-card"
-        className="pb-6 md:pt-12 md:pb-12 pt-6 max-w-5xl mx-auto relative z-10"
-      >
-        <div className="flex items-baseline space-x-4">
-          <h2 className="text-lg md:text-xl lg:text-2xl font-bold flex items-center">
-            What&#39;s on in Sydney
-          </h2>
-          <p>
-            <Link
-              href="/au/Sydney/discover/all"
-              className="text-orange-500 space-x-1"
-            >
-              <span className="font-semibold">View all</span>
-            </Link>
-          </p>
-        </div>
-        <div className="grid gap-3 lg:grid-cols-4 py-4">
-          {CARD_DETAILS.map((items, key) => (
-            <div className="w-full rounded-lg shadow-md lg:max-w-sm" key={key}>
-              <div className="overflow-hidden rounded-t-lg">
-                <img
-                  className="m-auto h-auto w-full transform transition-transform group-hover:scale-110 hover:scale-110"
-                  src={items.img}
-                  alt="image"
-                />
-              </div>
-
-              <div className="p-3 relative">
-                <div className="absolute -mt-12 w-10">
-                  <time className="flex items-center justify-center bg-gray-50 text-center rounded-lg leading-none w-12 min-h-12 shadow">
-                    <div>
-                      <p className="text-xs text-orange-500 uppercase rounded-t-lg">
-                        {items.month}
-                      </p>
-                      <p className="text-lg font-semibold">{items.date}</p>
-                    </div>
-                  </time>
+      <div className="px-4 md:px-0">
+        <section
+          id="carousel-card"
+          className="pb-6 md:pt-12 md:pb-12 pt-6 max-w-5xl mx-auto relative z-10"
+        >
+          <div className="flex items-baseline space-x-4">
+            <h2 className="text-lg md:text-xl lg:text-2xl font-bold flex items-center">
+              What&#39;s on in Sydney
+            </h2>
+            <p>
+              <Link
+                href="/au/Sydney/discover/all"
+                className="text-orange-500 space-x-1"
+              >
+                <span className="font-semibold">View all</span>
+              </Link>
+            </p>
+          </div>
+          <div className="grid gap-3 lg:grid-cols-4 py-4">
+            {CARD_DETAILS.map((items, key) => (
+              <div
+                className="w-full rounded-lg shadow-md lg:max-w-sm"
+                key={key}
+              >
+                <div className="overflow-hidden rounded-t-lg">
+                  <img
+                    className="m-auto h-auto w-full transform transition-transform group-hover:scale-110 hover:scale-110"
+                    src={items.img}
+                    alt="image"
+                  />
                 </div>
-                <div className="absolute top-0 right-0 m-1.5">
-                  <div>
-                    <button
-                      className="h-10 w-10 rounded-full hover:bg-opacity-100 bg-gray-900 bg-opacity-90"
-                      data-e2e="components/follow:button"
-                    >
-                      <div className="text-white absolute top-1/2 left-1/2 mx-auto -mt-3 -ml-3 w-6">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                          />
-                        </svg>
+
+                <div className="p-3 relative">
+                  <div className="absolute -mt-12 w-10">
+                    <time className="flex items-center justify-center bg-gray-50 text-center rounded-lg leading-none w-12 min-h-12 shadow">
+                      <div>
+                        <p className="text-xs text-orange-500 uppercase rounded-t-lg">
+                          {items.month}
+                        </p>
+                        <p className="text-lg font-semibold">{items.date}</p>
                       </div>
-                    </button>
+                    </time>
                   </div>
+                  <div className="absolute top-0 right-0 m-1.5">
+                    <div>
+                      <button
+                        className="h-10 w-10 rounded-full hover:bg-opacity-100 bg-gray-900 bg-opacity-90"
+                        data-e2e="components/follow:button"
+                      >
+                        <div className="text-white absolute top-1/2 left-1/2 mx-auto -mt-3 -ml-3 w-6">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                            />
+                          </svg>
+                        </div>
+                      </button>
+                    </div>
+                  </div>
+                  <p className="pt-1.5 font-bold">{items.title}</p>
+                  <p className=" text-gray-500 text-sm">{items.place}</p>
                 </div>
-                <p className="pt-1.5 font-bold">{items.title}</p>
-                <p className=" text-gray-500 text-sm">{items.place}</p>
-              </div>
-              <div className="p-3 mt-auto flex-none pt-0">
-                <Link
-                  href="#!"
-                  className="font-semibold text-orange-500 inline-block"
-                >
-                  Tickets from {items.price}
-                </Link>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
-      <section
-        id="trends-card"
-        className="pb-6 md:pt-12 md:pb-12 pt-6 max-w-5xl mx-auto relative z-10"
-      >
-        <div className="flex items-baseline space-x-4">
-          <h2 className="text-lg md:text-xl lg:text-2xl font-bold flex items-center">
-            What&#39;s Hot
-          </h2>
-        </div>
-        <div className="grid gap-3 lg:grid-cols-4 py-4">
-          {HOT_CARD_DETAILS.map((items, key) => (
-            <div className="w-full rounded-lg shadow-md lg:max-w-sm" key={key}>
-              <div className="overflow-hidden rounded-t-lg">
-                <img
-                  className="m-auto h-auto w-full transform transition-transform group-hover:scale-110 hover:scale-110"
-                  src={items.img}
-                  alt="image"
-                />
-              </div>
-
-              <div className="p-3 relative">
-                <p className="text-lg">
-                  <Link href="/k/ampol-state-of-origin" className="font-bold">
-                    Ampol State of Origin
+                <div className="p-3 mt-auto flex-none pt-0">
+                  <Link
+                    href="#!"
+                    className="font-semibold text-orange-500 inline-block"
+                  >
+                    Tickets from {items.price}
                   </Link>
-                </p>
-                <p className="text-sm">3 upcoming events</p>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </section>
-      <section
-        className="pb-6 md:pt-12 md:pb-12 pt-6 max-w-5xl mx-auto relative z-10"
-      >
-        <div className="flex items-baseline space-x-4">
-          <h2 className="text-lg md:text-xl lg:text-2xl font-bold flex items-center">
-            What&#39;s Hot
-          </h2>
-        </div>
-        <div className="flex items-baseline space-x-4 py-4">
-          {CATEGORIES.map(({name, imgUrl}, i)=>(<div key={i} className="flex-none pr-3 w-44 md:w-1/6">
-            <a
-              href="#"
-              title={name}
-            >
-              <div className="aspect-h-3 aspect-w-4 mb-2 overflow-hidden rounded-lg">
-                <img
-                  src={`https://tixel.com/cdn-cgi/image/width=400,format=auto/images/genres/${imgUrl}.png`}
-                  className="absolute my-auto h-auto w-full"
-                />
+            ))}
+          </div>
+        </section>
+        <section
+          id="trends-card"
+          className="pb-6 md:pt-12 md:pb-12 pt-6 max-w-5xl mx-auto relative z-10"
+        >
+          <div className="flex items-baseline space-x-4">
+            <h2 className="text-lg md:text-xl lg:text-2xl font-bold flex items-center">
+              What&#39;s Hot
+            </h2>
+          </div>
+          <div className="grid gap-3 lg:grid-cols-4 py-4">
+            {HOT_CARD_DETAILS.map((items, key) => (
+              <div
+                className="w-full rounded-lg shadow-md lg:max-w-sm"
+                key={key}
+              >
+                <div className="overflow-hidden rounded-t-lg">
+                  <img
+                    className="m-auto h-auto w-full transform transition-transform group-hover:scale-110 hover:scale-110"
+                    src={items.img}
+                    alt="image"
+                  />
+                </div>
+
+                <div className="p-3 relative">
+                  <p className="text-lg">
+                    <Link href="/k/ampol-state-of-origin" className="font-bold">
+                      Ampol State of Origin
+                    </Link>
+                  </p>
+                  <p className="text-sm">3 upcoming events</p>
+                </div>
               </div>
-              <p>
-                <strong>{name}</strong>
-              </p>
-            </a>
-          </div>))}
-        </div>
-      </section>
+            ))}
+          </div>
+        </section>
+        <section className="pb-6 md:pt-12 md:pb-12 pt-6 max-w-5xl mx-auto relative z-10">
+          <div className="flex items-baseline space-x-4">
+            <h2 className="text-lg md:text-xl lg:text-2xl font-bold flex items-center">
+              Browse by Category
+            </h2>
+          </div>
+          <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-6 py-4">
+            {CATEGORIES.map(({ name, imgUrl }, i) => (
+              <div key={i} className="flex-none">
+                <a href="#" title={name}>
+                  <div className="aspect-h-3 aspect-w-4 mb-2 overflow-hidden rounded-lg">
+                    <img
+                      src={`https://tixel.com/cdn-cgi/image/width=400,format=auto/images/genres/${imgUrl}.png`}
+                      className="absolute my-auto h-auto w-full"
+                    />
+                  </div>
+                  <p>
+                    <strong>{name}</strong>
+                  </p>
+                </a>
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
       <footer className="bg-gray-50">
         <div className="mx-auto max-w-5xl px-5">
           <div className="pt-16 pb-4 md:flex md:flex-wrap lg:pb-16">
